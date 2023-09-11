a4cfbd0de7dabe442769eb8ad09d56542e6f0d4b
chinedu06 <godswillchinedu644@gmail.com>
2023-09-11 12:52:23 -0700

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This is the main function that
+ *              prints a message and returns 0.
+ *
+ * Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle\n");
 return (0);
 }
