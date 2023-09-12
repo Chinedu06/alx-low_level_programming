6976d9709b20dbd2dc26d258d4f5e630b1fcdf3e
chinedu06 <godswillchinedu644@gmail.com>
2023-09-11 20:11:30 -0700

    first commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+int main(void)
+{
+char letter = a;
+while (letter <= z)
+{
+putchar(letter);
+letter++;
+}
+putchar(n);
+return (0);
+}
---
+++ 2-print_alphabet.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+int main(void)
+{
+char letter = a;
+while (letter <= z)
+{
+putchar(letter);
+letter++;
+}
+putchar(n);
+return (0);
