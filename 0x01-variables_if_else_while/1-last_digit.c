d620f67a317a44f3671f0c73449a7a1977cbd8f9
chinedu06 <godswillchinedu644@gmail.com>
2023-09-21 12:35:50 -0700

    first commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>   /* Include for srand and rand functions */
-#include <time.h>     /* Include for time function */
+#include <stdlib.h>
+#include <time.h>
 int main(void)
 {
 int n;
-srand(time(0));   /* Seed the random number generator */
+int lastDigit;
+srand(time(0));
 n = rand() - RAND_MAX / 2;
-int lastDigit = n % 10;
+lastDigit = n % 10;
 printf("Last digit of %d is %d and is ", n, lastDigit);
 if (lastDigit > 5)
 {
