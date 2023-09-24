13db578a33c9cce521a8722b05a7146325382e3e
chinedu06 <godswillchinedu644@gmail.com>
2023-09-23 17:50:23 -0700

    first commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,24 @@
 */
 void jack_bauer(void)
 {
-int hour, minute;
+int h1, h2, m1, m2;
 
-for (hour = 0; hour < 24; hour++)
+for (h1 = 0; h1 <= 2; h1++)
 {
-for (minute = 0; minute < 60; minute++)
+for (h2 = 0; h2 <= 9; h2++)
 {
-_putchar('0' + (hour / 10));
-_putchar('0' + (hour % 10));
+for (m1 = 0; m1 <= 5; m1++)
+{
+for (m2 = 0; m2 <= 9; m2++)
+{
+_putchar(h1 + '0');
+_putchar(h2 + '0');
 _putchar(':');
-_putchar('0' + (minute / 10));
-_putchar('0' + (minute % 10));
+_putchar(m1 + '0');
+_putchar(m2 + '0');
 _putchar('\n');
+}
+}
 }
 }
 }
