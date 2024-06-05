0ff04867c2d240cf7c5566a6bc42a0f3d416ec6f
chinedu06 <godswillchinedu644@gmail.com>
2024-06-04 20:14:50 -0700

    first commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -22,13 +22,3 @@ int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
-
-
-#ifndef MAIN_H
-#define MAIN_H
-
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-
-#endif /* MAIN_H */
-
