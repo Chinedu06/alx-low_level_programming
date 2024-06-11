1a4349a0ac9f9e88ec2eead7cabd8eeb172953dd
chinedu06 <godswillchinedu644@gmail.com>
2024-06-11 14:38:55 -0700

    first commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,13 +4,14 @@
  * _atoi - Convert a string to an integer.
  * @s: The pointer to the string to be converted.
  *
- * Return:The converted integer.If there are no numbers in the string,return0
+ * Return: The converted integer. If there are no numbers in the string, return 0.
  */
 int _atoi(char *s)
 {
 	int result = 0;
 	int sign = 1;
 	int started = 0;
+	int digit;
 
 	while (*s != '\0')
 	{
@@ -24,8 +25,16 @@ int _atoi(char *s)
 		}
 		else if (*s >= '0' && *s <= '9')
 		{
+			digit = *s - '0';
+			if (result > (2147483647 - digit) / 10)
+			{
+				if (sign == 1)
+					return 2147483647;
+				else
+					return -2147483648;
+			}
+			result = result * 10 + digit;
 			started = 1;
-			result = result * 10 + (*s - '0');
 		}
 		else if (started)
 		{
@@ -33,7 +42,7 @@ int _atoi(char *s)
 		}
 		s++;
 	}
-
+	
 	return (result * sign);
 }
 
