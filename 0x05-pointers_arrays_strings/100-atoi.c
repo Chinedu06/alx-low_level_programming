e4e345d949544c4167ef2da2560ce0ca250cd66b
chinedu06 <godswillchinedu644@gmail.com>
2024-06-11 13:01:09 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the converted integer
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	/*Handle the sign*/
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+
+	/*Convert the digits*/
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+	{
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
+
+	/*Apply the sign*/
+	return (result * sign);
+}
+
