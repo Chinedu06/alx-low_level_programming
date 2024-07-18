2e2e1395b5f339ceac2031ca48ce14e0651a00aa
root <chinedu06>
2024-07-18 06:49:22 -0700

    first commit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,9 +4,9 @@
 #include <stddef.h>  /* Include for size_t */
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t left, size_t right, int value); /* bin */
+int binary_search(int *array, size_t left, size_t right, int value);
 int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value); /* inter */
+int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
