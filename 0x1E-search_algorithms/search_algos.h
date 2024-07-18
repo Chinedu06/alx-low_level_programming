416846f0d970bbb5badd714699353ef0f9148fa1
root <chinedu06>
2024-07-18 13:27:11 -0700

    first commit

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,26 +1,24 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <stddef.h>  /* Include for size_t */
-#include <stdio.h>   /* Include for printf */
+#include <stddef.h>
+#include <stdio.h>
 
-/* Structure for a singly linked list with an express lane */
 /**
  * struct skiplist_s - Singly linked list with an express lane
- *
- * @n: Integer
+ * @n: Integer stored in the node
  * @index: Index of the node in the list
- * @next: Pointer to the next node
+ * @next: Pointer to the next node in the list
  * @express: Pointer to the next node in the express lane
  *
  * Description: singly linked list node structure with an express lane
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
@@ -31,7 +29,8 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
-/* Declaration of free_skiplist function */
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
 void free_skiplist(skiplist_t *list);
 
 #endif /* SEARCH_ALGOS_H */
