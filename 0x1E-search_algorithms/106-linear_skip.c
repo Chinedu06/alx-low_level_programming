4f0cc1c708d33e91b89f86620689bfbc0613f690
root <chinedu06>
2024-07-18 13:15:46 -0700

    first commit

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
 #include "search_algos.h"
+#include <stdio.h>
+#include <math.h>
 
 /**
- * linear_skip - searches for a value in a sorted skip list of integers
- * @list: pointer to the head of the skip list to search in
- * @value: the value to search for
+ * linear_skip - Searches for a value in a sorted skip list of integers.
+ * @list: A pointer to the head of the skip list to search in.
+ * @value: The value to search for.
  *
- * Return: pointer on the first node where value is located, or NULLifnotfound
+ * Return: A pointer on the first node where value is located, or NULL if
+ *         value is not present in list or if head is NULL.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
 	skiplist_t *prev, *curr;
 
-	if (list == NULL)
+	if (!list)
 		return (NULL);
 
 	prev = list;
@@ -34,13 +36,13 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	}
 
 	printf("Valuefoundbetweenindexes [%lu]and[%lu]\n", prev->index, curr->index);
-
-	while (prev && prev->index <= curr->index)
+	curr = prev;
+	while (curr != NULL && curr->index <= prev->index)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
-		if (prev->n == value)
-			return (prev);
-		prev = prev->next;
+		printf("Value checked at index [%lu] = [%d]\n", curr->index, curr->n);
+		if (curr->n == value)
+			return (curr);
+		curr = curr->next;
 	}
 
 	return (NULL);
