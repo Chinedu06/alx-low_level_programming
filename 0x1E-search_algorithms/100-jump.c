4fb59f0c306481f12aa512f50baec84f478fe88f
root <chinedu06>
2024-07-19 05:01:26 -0700

    first commit

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -4,16 +4,19 @@
 #include <math.h>
 
 /**
- * jump_search-searchesforavalueinasortedarrayofintegersusingJumpsearchalgo
+ * jump_search - searches for a value in a sorted array using Jump search algo
  * @array: A pointer to the first element of the array to search in
  * @size: The number of elements in array
  * @value: The value to search for
  *
- * Return:Thefirstindexwherevalueislocated,or-1ifvalueisnotpresentorarrayisNULL
+ * Return: The first index where value is located, or -1 if value is not
+ *         present or array is NULL
  */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t step, prev, i;
+	size_t step;
+	size_t prev;
+	size_t i;
 	size_t sqrt_size;
 
 	if (array == NULL || size == 0)
@@ -27,13 +30,16 @@ int jump_search(int *array, size_t size, int value)
 	step = sqrt_size;
 	prev = 0;
 
-	while (prev + step < size && array[prev + step] < value)
+	while (prev < size && array[prev] < value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+		if (prev + step >= size || array[prev + step] >= value)
+			break;
 		prev += step;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev, prev + step);
+	printf("Value found between indexes [%lu] and [%lu]\n", prev,
+	       (prev + step < size) ? prev + step : size - 1);
 
 	for (i = prev; i < size && i <= prev + step; i++)
 	{
