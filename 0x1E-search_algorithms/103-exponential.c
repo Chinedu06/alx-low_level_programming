78caa64ed94935213af09f421270fb1fc55881eb
root <chinedu06>
2024-07-19 10:08:51 -0700

    first commit

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_search - Searches for a value in a sorted array of integers using
- *		   the Binary Search algorithm.
+ *                 the Binary Search algorithm.
  * @array: A pointer to the first element of the array to search.
  * @left: The starting index of the subarray to search.
  * @right: The ending index of the subarray to search.
@@ -17,29 +17,29 @@ int binary_search(int *array, size_t left, size_t right, int value)
 
     while (left <= right)
     {
-	mid = left + (right - left) / 2;
-	printf("Searching in array: ");
-	for (i = left; i <= right; i++)
-	{
-	    printf("%d", array[i]);
-	    if (i < right)
-		printf(", ");
-	}
-	printf("\n");
+        mid = left + (right - left) / 2;
+        printf("Searching in array: ");
+        for (i = left; i <= right; i++)
+        {
+            printf("%d", array[i]);
+            if (i < right)
+                printf(", ");
+        }
+        printf("\n");
 
-	if (array[mid] == value)
-	    return (mid);
-	if (array[mid] < value)
-	    left = mid + 1;
-	else
-	    right = mid - 1;
+        if (array[mid] == value)
+            return (mid);
+        if (array[mid] < value)
+            left = mid + 1;
+        else
+            right = mid - 1;
     }
     return (-1);
 }
 
 /**
  * exponential_search - Searches for a value in a sorted array of integers
- *			using the Exponential Search algorithm.
+ *                      using the Exponential Search algorithm.
  * @array: A pointer to the first element of the array to search.
  * @size: The number of elements in the array.
  * @value: The value to search for.
@@ -53,15 +53,15 @@ int exponential_search(int *array, size_t size, int value)
     size_t right;
 
     if (array == NULL)
-	return (-1);
+        return (-1);
 
     if (array[0] == value)
-	return (0);
+        return (0);
 
     while (bound < size && array[bound] < value)
     {
-	printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
-	bound *= 2;
+        printf("Value checked array[%lu] = [%d]\n", bound, array[bound]);
+        bound *= 2;
     }
 
     left = bound / 2;
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,26 +5,15 @@
 #include <stdio.h>
 #include <math.h>
 
-/**
- * struct skiplist_s - Singly linked list with an express lane
- * @n: Integer stored in the node
- * @index: Index of the node in the list
- * @next: Pointer to the next node in the list
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node structure with an express lane
- */
 typedef struct skiplist_s
 {
-        int n;
-        size_t index;
-        struct skiplist_s *next;
-        struct skiplist_s *express;
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-/* Removed C++ style comment */
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
